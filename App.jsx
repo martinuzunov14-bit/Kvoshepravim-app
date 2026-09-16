cf28bec55db19bc8058c561cbcb519941a6a2aa2
martinuzunov14-bit <martin.uzunov14@gmail.com>
2026-09-16 15:30:38 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-limport React, { useState, useMemo, useEffect, useRef } from "react";
+import React, { useState, useMemo, useEffect, useRef } from "react";
 import { supabase } from "./supabaseClient.js";
 
 /* ============================================================================
